0d74122a061ce9cf0d4da4c7f1b023d0644055e1
mbourgeois(Arsenic) <marc.bourgeois77@gmail.com>
2020-06-17 15:32:37 -0500

    updated input file for single NP example

--- GRPF_python/analysis_parameters.m
+++ GRPF_python/analysis_parameters.m
@@ -1,15 +1,15 @@
 
 
 % rectangular domain definition z=x+jy x\in[xb,xe] , y\in[yb,ye]
-xb = 2.4635*0.9;     % real part range begin 
-xe = 2.4635*1.1 ;     % real part range end
+xb = 3.0;     % real part range begin 
+xe = 4.0 ;     % real part range end
 yb = -0.1;     % imag part range begin
 ye = 0;      % imag part range end 
 r = 0.01;     % initial mesh step
 
 NewNodesCoord = rect_dom(xb,xe,yb,ye,r); % initial mesh generation
 
-Tol = 1e-5; % accuracy (candidate region size)
+Tol = 1e-4; % accuracy (candidate region size)
 
 visual=0; % mesh visualization:  0 - turned off,   1  - only last iteration,   2  - all iterations
 
@@ -24,19 +24,10 @@ SkinnyTriangle=3; % sinny triangle definition
 %	Lattice Parameters	%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-K_parallel = 9.519978e-03; % in-plane wave vector [nm^-1]
 
-RNP  = 20.000000;	% nanoparticle radius [nm]
+RNP  = 25.000000;	% nanoparticle radius [nm]
 
-Nind = 1.500000;	% background refractive index
-
-d    = 330.000000;	% unit cell length [nm]
-
-t    = 165.000000;	% spacing between NPs within the unit cell [nm]
-
-
-
-
+Nind = 1.000000;	% background refractive index
 
 
 
